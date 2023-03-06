8469084b7d0202d3a213451783f2d447ac997f5c
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 08:18:12 +0530

    Add files via upload

---
+++ logicopperator.php
@@ -0,0 +1,13 @@
+<html>
+    <?php
+    $year=2012;
+    if($year%4==0)
+    {
+        print($year." is a leap year.");
+    }
+    else
+    {
+        print($year."is not a leap year.");
+    }
+    ?>
+</html>
